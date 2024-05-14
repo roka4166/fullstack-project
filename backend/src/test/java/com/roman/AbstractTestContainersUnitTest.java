53e20ec12e90f3f3130cd47b3d6d154d1e2d933e
roka4166 <mrrubox1@gmail.com>
2024-05-15 01:25:26 +0200

    test

--- backend/src/test/java/com/roman/AbstractTestContainersUnitTest.java
+++ backend/src/test/java/com/roman/AbstractTestContainersUnitTest.java
@@ -10,7 +10,6 @@ import org.springframework.test.context.DynamicPropertySource;
 import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
-
 import javax.sql.DataSource;
 
 @Testcontainers
