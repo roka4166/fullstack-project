93d15c11fb5eaa664d263b49eb2b42c05dd20338
roka4166 <mrrubox1@gmail.com>
2024-05-15 00:15:56 +0200

    starting with github workflows

--- backend/src/main/java/com/roman/Main.java
+++ backend/src/main/java/com/roman/Main.java
@@ -2,8 +2,6 @@ package com.roman;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
@@ -11,9 +9,5 @@ import org.springframework.web.bind.annotation.RestController;
 public class Main {
     public static void main(String[] args) {
         SpringApplication.run(Main.class, args);
-    }
-    @GetMapping("/api/v1/customer")
-    public String getName(){
-        return "Roman";
     }
 }
