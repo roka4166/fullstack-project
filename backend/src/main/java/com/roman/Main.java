cc74340d919306531a56125c816f0aaab53d15c1
roka4166 <mrrubox1@gmail.com>
2024-06-13 16:24:20 +0200

    slack message test 2

--- backend/src/main/java/com/roman/Main.java
+++ backend/src/main/java/com/roman/Main.java
@@ -6,6 +6,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class Main {
+
+    
     public static void main(String[] args) {
         SpringApplication.run(Main.class, args);
     }
